632343dccf3d06a0f86e90e94026de6b870df4d1
the3reze <ereze.ahmetgjekaj@gmail.com>
2019-11-13 15:15:16 +0100

    styles and reports

--- public/themes/public/layouts/home.blade.php
+++ public/themes/public/layouts/home.blade.php
@@ -10,22 +10,24 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- Google Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,500,600,700,700i|Montserrat:300,400,500,600,700" rel="stylesheet">
-
+        <link href="https://fonts.googleapis.com/css?family=Poppins:400,700" rel="stylesheet">
         <!-- Bootstrap CSS File -->
         <link href="themes/public/assets/custom/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
         <!-- Libraries CSS Files -->
-        <link href="themes/public/assets/custom/lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-        <link href="themes/public/assets/custom/lib/animate/animate.min.css" rel="stylesheet">
-        <link href="themes/public/assets/custom/lib/ionicons/css/ionicons.min.css" rel="stylesheet">
-        <link href="themes/public/assets/custom/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
-        <link href="themes/public/assets/custom/lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="themes/public/assets/maintheme/assets/vendors/liquid-icon/liquid-icon.min.css" />
+        <link rel="stylesheet" href="themes/public/assets/maintheme/assets/vendors/font-awesome/css/font-awesome.min.css" />
+        <link rel="stylesheet" href="themes/public/assets/maintheme/assets/css/theme-vendors.min.css" />
+        <link rel="stylesheet" href="themes/public/assets/maintheme/assets/css/theme.min.css" />
+        <link rel="stylesheet" href="themes/public/assets/maintheme/assets/css/themes/creative.css" />
 
         <!-- Main Stylesheet File -->
-        <link href="themes/public/assets/custom/css/style.css" rel="stylesheet">
+{{--        <link href="themes/public/assets/custom/css/style.css" rel="stylesheet">--}}
         {{--{!! Theme::asset()->styles() !!}--}}
         {{--{!! Theme::asset()->scripts() !!}--}}
+        <script async src="themes/public/assets/maintheme/assets/vendors/modernizr.min.js"></script>
     </head>
+
 
     <body>
 
@@ -39,21 +41,10 @@
         <!-- <div id="preloader"></div> -->
 
         <!-- JavaScript Libraries -->
-        <script src="themes/public/assets/custom/lib/jquery/jquery.min.js"></script>
-        <script src="themes/public/assets/custom/lib/jquery/jquery-migrate.min.js"></script>
-        <script src="themes/public/assets/custom/lib/bootstrap/js/bootstrap.bundle.min.js"></script>
-        <script src="themes/public/assets/custom/lib/easing/easing.min.js"></script>
-        <script src="themes/public/assets/custom/lib/mobile-nav/mobile-nav.js"></script>
-        <script src="themes/public/assets/custom/lib/wow/wow.min.js"></script>
-        <script src="themes/public/assets/custom/lib/waypoints/waypoints.min.js"></script>
-        <script src="themes/public/assets/custom/lib/counterup/counterup.min.js"></script>
-        <script src="themes/public/assets/custom/lib/owlcarousel/owl.carousel.min.js"></script>
-        <script src="themes/public/assets/custom/lib/isotope/isotope.pkgd.min.js"></script>
-        <script src="themes/public/assets/custom/lib/lightbox/js/lightbox.min.js"></script>
+        <script src="themes/public/assets/maintheme/assets/vendors/jquery.min.js"></script>
+        <script src="themes/public/assets/maintheme/assets/js/theme-vendors.js"></script>
+        <script src="themes/public/assets/maintheme/assets/js/theme.min.js"></script>cript>
         <!-- Contact Form JavaScript File -->
-        <script src="themes/public/assets/custom/contactform/contactform.js"></script>
 
-        <!-- Template Main Javascript File -->
-        <script src="themes/public/assets/custom/js/main.js"></script>
     </body>
 </html>
--- public/themes/public/partials/header.blade.php
+++ public/themes/public/partials/header.blade.php
@@ -40,32 +40,150 @@
 {{--</header>--}}
 
 
-    <header id="header">
+{{--    <header id="header">--}}
 
-        <div id="topbar">
-            <div class="container">
-                <div class="social-links">
-                    <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
-                    <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
-                    <a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a>
-                    <a href="#" class="instagram"><i class="fa fa-instagram"></i></a>
-                </div>
-            </div>
-        </div>
+{{--        <div id="topbar">--}}
+{{--            <div class="container">--}}
+{{--                <div class="social-links">--}}
+{{--                    <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>--}}
+{{--                    <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>--}}
+{{--                    <a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a>--}}
+{{--                    <a href="#" class="instagram"><i class="fa fa-instagram"></i></a>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
 
-        <div class="container">
+{{--        <div class="container">--}}
 
-            <div class="logo float-left">
-                <!-- Uncomment below if you prefer to use an image logo -->
-                <h1 class="text-light"><a href="#intro" class="scrollto"><span>Mentor</span></a></h1>
-                <!-- <a href="#header" class="scrollto"><img src="img/logo.png" alt="" class="img-fluid"></a> -->
-            </div>
+{{--            <div class="logo float-left">--}}
+{{--                <!-- Uncomment below if you prefer to use an image logo -->--}}
+{{--                <h1 class="text-light"><a href="#intro" class="scrollto"><span>Mentor</span></a></h1>--}}
+{{--                <!-- <a href="#header" class="scrollto"><img src="img/logo.png" alt="" class="img-fluid"></a> -->--}}
+{{--            </div>--}}
 
-            <nav class="main-nav float-right d-none d-lg-block">
-                <ul>
-                    {!!Menu::menu('main')!!}
-                </ul>
-            </nav><!-- .main-nav -->
+{{--            <nav class="main-nav float-right d-none d-lg-block">--}}
+{{--                <ul>--}}
+{{--                    {!!Menu::menu('main')!!}--}}
+{{--                </ul>--}}
+{{--            </nav><!-- .main-nav -->--}}
 
-        </div>
-    </header><!-- #header -->
+{{--        </div>--}}
+{{--    </header><!-- #header -->--}}
+
+
+    <header class="main-header main-header-overlay" data-sticky-header="true" data-sticky-options='{ "stickyTrigger": "first-section" }'>
+
+        <div class="mainbar-wrap">
+            <div class="megamenu-hover-bg"></div><!-- /.megamenu-hover-bg -->
+            <div class="container-fluid mainbar-container">
+                <div class="mainbar">
+                    <div class="row mainbar-row align-items-lg-stretch px-4">
+
+                        <div class="col-auto pr-5">
+                            <div class="navbar-header">
+                                <a class="navbar-brand" href="index-creative.html" rel="home">
+										<span class="navbar-brand-inner">
+											<img class="logo-dark" src="./assets/img/logo/logo-1.svg" alt="Ave HTML Template">
+											<img class="logo-sticky" src="./assets/img/logo/logo-1.svg" alt="Ave HTML Template">
+											<img class="mobile-logo-default" src="./assets/img/logo/logo-1.svg" alt="Ave HTML Template">
+											<img class="logo-default" src="./assets/img/logo/logo-1.svg" alt="Ave HTML Template">
+										</span>
+                                </a>
+                                <button type="button" class="navbar-toggle collapsed nav-trigger style-mobile" data-toggle="collapse" data-target="#main-header-collapse" aria-expanded="false" data-changeclassnames='{ "html": "mobile-nav-activated overflow-hidden" }'>
+                                    <span class="sr-only">Toggle navigation</span>
+                                    <span class="bars">
+											<span class="bar"></span>
+											<span class="bar"></span>
+											<span class="bar"></span>
+										</span>
+                                </button>
+                            </div><!-- /.navbar-header -->
+                        </div><!-- /.col -->
+
+                        <div class="col">
+
+                            <div class="collapse navbar-collapse" id="main-header-collapse">
+
+                                <ul id="primary-nav" class="main-nav nav align-items-lg-stretch justify-content-lg-start" data-submenu-options='{ "toggleType":"fade", "handler":"mouse-in-out" }' data-localscroll="true">
+
+                                    <li>
+                                        <a href="#content">
+                                            <span class="link-icon"></span>
+                                            <span class="link-txt">
+													<span class="link-ext"></span>
+													<span class="txt">Home</span>
+												</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#works">
+                                            <span class="link-icon"></span>
+                                            <span class="link-txt">
+													<span class="link-ext"></span>
+													<span class="txt">Works</span>
+												</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#services">
+                                            <span class="link-icon"></span>
+                                            <span class="link-txt">
+													<span class="link-ext"></span>
+													<span class="txt">Services</span>
+												</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#about">
+                                            <span class="link-icon"></span>
+                                            <span class="link-txt">
+													<span class="link-ext"></span>
+													<span class="txt">About</span>
+												</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#testimonials">
+                                            <span class="link-icon"></span>
+                                            <span class="link-txt">
+													<span class="link-ext"></span>
+													<span class="txt">Testimonials</span>
+												</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#news">
+                                            <span class="link-icon"></span>
+                                            <span class="link-txt">
+													<span class="link-ext"></span>
+													<span class="txt">News</span>
+												</span>
+                                        </a>
+                                    </li>
+
+                                </ul><!-- /#primary-nav  -->
+
+                            </div><!-- /#main-header-collapse -->
+
+                        </div><!-- /.col -->
+
+                        <div class="col text-right">
+
+                            <div class="header-module">
+
+                                <a href="#" target="_blank" class="btn btn-default text-uppercase circle btn-bordered border-thick font-size-12 font-weight-semibold btn-white">
+									<span>
+										<span class="btn-txt">Join to Download</span>
+									</span>
+                                </a>
+
+                            </div><!-- /.header-module -->
+
+                        </div><!-- /.col -->
+
+                    </div><!-- /.mainbar-row -->
+                </div><!-- /.mainbar -->
+            </div><!-- /.mainbar-container -->
+        </div><!-- /.mainbar-wrap -->
+
+    </header><!-- /.main-header -->
