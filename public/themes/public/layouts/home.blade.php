5fb7ae006737033720394b63d0c5c789f0ca8351
the3reze <ereze.ahmetgjekaj@gmail.com>
2019-11-28 16:37:25 +0100

    upload pic button removal, meta tittle

--- public/themes/public/layouts/home.blade.php
+++ public/themes/public/layouts/home.blade.php
@@ -5,6 +5,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
 {{--        <title>{{ Theme::getMetaTitle() }} - {{__('app.name')}}</title>--}}
+        <title>Mentorship IV Youth</title>
         <meta name="description" content="{{ Theme::getMetaDesctiption() }}">
         <meta name="keyword" content="{{ Theme::getMetaKeyword() }}">
         <meta name="viewport" content="width=device-width, initial-scale=1">
